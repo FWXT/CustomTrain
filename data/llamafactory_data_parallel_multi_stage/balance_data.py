60b4ef45f417dc4514549d319948c6261a7a1b0b
FWXT <152080362+FWXT@users.noreply.github.com>
2025-07-26 15:59:05 +0800

    Update balance_data.py
    增加正负样本可视化功能，增加负样本丢弃概率

--- data/llamafactory_data_parallel_multi_stage/balance_data.py
+++ data/llamafactory_data_parallel_multi_stage/balance_data.py
@@ -6,24 +6,41 @@ input_file = "/data/mnt_bucket/qzq/CustomTrain/data/llamafactory_data_parallel_m
 output_file = "/data/mnt_bucket/qzq/CustomTrain/data/llamafactory_data_parallel_multi_stage/stage_scale_2/balanced_necessary_train_zeta.json"
 random.seed(42)
 # 读取 JSON 数据
-with open(input_file, "r", encoding="utf-8") as f:
-    data = json.load(f)
 
-    sample_data = []
-    for item in data:
-        if item["is_unchanged"]:
-            # random prob less than 1/2, keep it
-            if random.random() < 0.333:
+def inpsect_unchange_ratio(input_file):
+    stat = {}
+    with open(input_file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        count = 0
+        unchange_count = 0
+        for item in data:
+            count+=1
+            if item["is_unchanged"]:
+                unchange_count += 1
+    stat["all"] = count
+    stat["is_unchanged"] = unchange_count
+    print("unchange ratio:", 1.0*unchange_count/count)
+    return stat
+
+def balance_data(input_file, output_file, negative_keep_prob:float=0.333):
+    with open(input_file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+        sample_data = []
+        for item in data:
+            if item["is_unchanged"]:
+                # random prob less than 1/2, keep it
+                if random.random() < negative_keep_prob:
+                    sample_data.append(item)
+            else:
                 sample_data.append(item)
-        else:
-            sample_data.append(item)
 
-    data = sample_data
+        data = sample_data
 
+    # 保存到新文件，确保中文不被转义
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(sample_data, f, ensure_ascii=False, indent=2)
 
-# 取前 800 个样本
-
-# 保存到新文件，确保中文不被转义
-with open(output_file, "w", encoding="utf-8") as f:
-    json.dump(sample_data, f, ensure_ascii=False, indent=2)
-
+if __name__ == "__main__":
+    stat = inpsect_unchange_ratio("/data1/public/CustomTrain/data/llamafactory_data_parallel_multi_stage/stage_scale_2/train_zeta.json")
+    print(stat)
